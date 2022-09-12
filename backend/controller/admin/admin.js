9a8da1b2c3f5668bca273f9ef80928126701c8f0
Ameen <ameendv00@gmail.com>
2022-09-12 23:05:50 +0530

    Second commit

--- backend/controller/admin/admin.js
+++ backend/controller/admin/admin.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
+// eslint-disable-next-line no-unused-vars
 const app = express()
 const Tasks = require('../../models/tasks')
 const Admin = require('../../models/admin')
@@ -24,81 +26,83 @@ module.exports = {
                 next(createError(401, 'Incorrect password'))
             }
         }).catch((error) => {
-            console.log(error)
+            next(createError())
         })
     },
     createTask: async (req, res, next) => {
-        
 
-            req.body.assignee = req.user.name
 
-            await Tasks.create(req.body).then((response) => {
+        req.body.assignee = req.user.name
 
-                res.status(200).json({ message: 'Task created successfully' })
-            }).catch((error)=>{
-                next(createError(401,'All data required'))
-            })
-        
+        await Tasks.create(req.body).then((response) => {
+
+            res.status(200).json({ message: 'Task created successfully' })
+        }).catch((error) => {
+            next(createError(401, 'All data required'))
+        })
+
     },
-    addTeam:async(req,res,next)=>{
-        
-            await Team.create(req.body).then((response)=>{
-                res.status(200).json({message:'Team created succesfully'})
-            }).catch((error)=>{
-                res.status(500)
-            })
-      
-    },
-    assignTask:async(req,res,next)=>{
-        if(!req.query.userId || !req.query.taskId){
-            return next(createError(400,'Users id and task id required to assign a task'))
-        }
+    addTeam: async (req, res, next) => {
 
-        await Tasks.updateOne({_id:req.query.taskId},{$set:{assignedTo:req.query.userId}}).then((response)=>{
-            res.status(200).json({message:'Task assigned to a team member'})
-        }).catch((error)=>{
+        await Team.create(req.body).then((response) => {
+            res.status(200).json({ message: 'Team created succesfully' })
+        }).catch((error) => {
             res.status(500)
         })
+
     },
-    getTasks:async(req,res)=>{
-        await Tasks.find().then((response)=>{
+    assignTask: async (req, res, next) => {
+        if (!req.query.userId || !req.query.taskId) {
+            return next(createError(400, 'Users id and task id required to assign a task'))
+        }
+
+        await Tasks.updateOne({ _id: req.query.taskId }, { $set: { assignedTo: req.query.userId } }).then((response) => {
+            res.status(200).json({ message: 'Task assigned to a team member' })
+        }).catch((error) => {
+            next(createError())
+        })
+    },
+    getTasks: async (req, res, next) => {
+        await Tasks.find().then((response) => {
             console.log(response)
-        }).catch((error)=>{
-            res.status(500)
+        }).catch((error) => {
+            next(createError())
         })
     },
-    editStatus:async(req,res,next)=>{
-        if(!req.query.id){
-            return next(createError(400,'Task id is required'))
+    editStatus: async (req, res, next) => {
+        if (!req.query.id) {
+            return next(createError(400, 'Task id is required'))
         }
-        await Tasks.updateOne({_id:req.query.id},{$set:{status:req.body.status}}).then((response)=>{
+        await Tasks.updateOne({ _id: req.query.id }, { $set: { status: req.body.status } }).then((response) => {
             console.log(response)
-            res.status(200).json({message:'Status updated succesfully'})
-        }).catch((error)=>{
-            res.status(500)
+            res.status(200).json({ message: 'Status updated succesfully' })
+        }).catch((error) => {
+            next(createError())
         })
     },
-    editProperties:async(req,res,next)=>{
-        
-        if(!req.query.id){
-            return next(createError(400,"Task's id needed to edit a task"))
+    editProperties: async (req, res, next) => {
+
+        if (!req.query.id) {
+            return next(createError(400, 'Task"s id needed to edit a task'))
         }
 
 
-       for(i of Object.values(req.body)){
-        if(i===''){
-            next(createError(401,'All fields required'))
-        }else{
-            await Tasks.updateOne({_id:req.query.id},{$set:{...req.body}}).then((response)=>{
-                console.log(response)
-            }).catch((error)=>{
-                console.log(error)
-            })
+        for (var i of Object.values(req.body)) {
+            if (i === '') {
+                next(createError(401, 'All fields required'))
+            } else {
+                await Tasks.updateOne({ _id: req.query.id }, { $set: { ...req.body } }).then((response) => {
+                    console.log(response)
+                    return res.status(200).json({ message: 'Properties edited.' })
+                }).catch((error) => {
+                    next(createError())
+                })
+            }
         }
-       }
     }
 }
 
 function generateAccessToken(user) {
+    // eslint-disable-next-line no-undef
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
 }
--- backend/routes/admin/admin.js
+++ backend/routes/admin/admin.js
@@ -1,41 +1,36 @@
-const express =require('express')
-const app=express()
-const adminController=require('../../controller/admin/admin')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const adminController = require('../../controller/admin/admin')
 const jwt = require('jsonwebtoken')
-const {createError} = require('../../createError/createError')
+const { createError } = require('../../createError/createError')
 
-app.post('/api/login',adminController.login)
+app.post('/api/login', adminController.login)
 
-app.post('/api/create-task',authenticateToken,adminController.createTask)
+app.post('/api/create-task', authenticateToken, adminController.createTask)
 
-app.post ('/api/add-team',authenticateToken,adminController.addTeam)
+app.post('/api/add-team', authenticateToken, adminController.addTeam)
 
-app.put('/api/assign-task',authenticateToken,adminController.assignTask)
+app.put('/api/assign-task', authenticateToken, adminController.assignTask)
 
-app.get('/api/get-tasks',authenticateToken,adminController.getTasks)
+app.get('/api/get-tasks', authenticateToken, adminController.getTasks)
 
-app.put('/api/edit-status',authenticateToken,adminController.editStatus)
+app.put('/api/edit-status', authenticateToken, adminController.editStatus)
 
-app.put('/api/edit-properties',authenticateToken,adminController.editProperties)
-
-
-
-
+app.put('/api/edit-properties', authenticateToken, adminController.editProperties)
 function authenticateToken(req, res, next) {
-    
+
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-
-
     if (token == null) return res.sendStatus(401)
-
+    // eslint-disable-next-line no-undef
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return next(createError(403,'Authentication failed'))
+        if (err) return next(createError(403, 'Authentication failed'))
         req.user = user
-       next()
+        next()
     })
 
 
 }
 
-module.exports=app;
+module.exports = app;
